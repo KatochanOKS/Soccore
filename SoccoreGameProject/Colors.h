54050da0d73d569bd92d378c1ed7838bf55e5ca9
katochan <ryosuke0909777@icloud.com>
2025-06-03 21:38:01 +0900

    めっちゃ途中、地面描画やゲームオブジェクトファイルを追加した

---
+++ SoccoreGameProject/Colors.h
@@ -0,0 +1,14 @@
+// Colors.h
+#pragma once
+#include <DirectXMath.h>
+using namespace DirectX;
+
+namespace Colors {
+    static const XMFLOAT4 White = XMFLOAT4(1, 1, 1, 1);
+    static const XMFLOAT4 Red = XMFLOAT4(1, 0, 0, 1);
+    static const XMFLOAT4 Green = XMFLOAT4(0, 1, 0, 1);
+    static const XMFLOAT4 Blue = XMFLOAT4(0, 0, 1, 1);
+    static const XMFLOAT4 Yellow = XMFLOAT4(1, 1, 0, 1);
+    static const XMFLOAT4 Gray = XMFLOAT4(0.5f, 0.5f, 0.5f, 1);
+    // ... çDÇ´Ç»ÇæÇØëùÇ‚ÇπÇÈ
+}
