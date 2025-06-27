ab20e97f7baad1a668db57b102686af3555a0a05
katochan <ryosuke0909777@icloud.com>
2025-06-27 11:19:32 +0900

    整理

--- SoccoreGameProject/FbxSkeletalLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "SkeletalMesh.h"
-#include "AnimationClip.h"
-
-class FbxSkeletalLoader {
-public:
-    static bool LoadMesh(const std::string& filePath, SkeletalMesh& meshOut);
-    static AnimationClip* LoadAnimation(const std::string& filePath, const std::vector<Bone>& bones);
-};
