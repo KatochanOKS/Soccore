475cdc3a5ea88402d0bb06fefd0c3eb20d88e5f2
katochan <ryosuke0909777@icloud.com>
2025-09-04 23:41:00 +0900

    動作確認

--- SoccoreGameProject/EngineManager.cpp
+++ SoccoreGameProject/EngineManager.cpp
@@ -3,9 +3,6 @@
 #include "MeshLibrary.h"
 #include"StartScene.h"
 #include <memory>
-#include "imgui.h"
-#include "imgui_impl_dx12.h"
-#include "imgui_impl_win32.h"
 
 void EngineManager::Initialize() {
 
